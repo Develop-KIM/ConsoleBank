8a94f97532afd53d9d99a0d6e519e752cc13cdaf
Kimdonghwan <kimdonghwan913@gmail.com>
2024-01-23 18:10:05 +0900

    final commit (2024/01/23)

--- src/save/AutoSaver.java
+++ src/save/AutoSaver.java
@@ -3,30 +3,30 @@ package save;
 import service.AccountService;
 
 public class AutoSaver extends Thread {
-    private AccountService accountService;
+	private AccountService accountService;
 
-    /**
-     * AutoSaver 객체를 생설하고 데몬 쓰레드로 설정
-     * @param accountService 계좌 서비스 객체
-     */
-    public AutoSaver(AccountService accountService) {
-        this.accountService = accountService;
-        setDaemon(true);  // 데몬 쓰레드로 설정
-    }
+	/**
+	 * AutoSaver 객체를 생성하고 데몬 쓰레드로 설정
+	 * 
+	 * @param accountService 계좌 서비스 객체
+	 */
+	public AutoSaver(AccountService accountService) {
+		this.accountService = accountService;
+		setDaemon(true); // 데몬 쓰레드로 설정
+	}
 
-    /**
-     * 계좌 정보를 자동으로 5초마다 저장
-     */
-    @Override
-    public void run() {
-        while (true) {
-            accountService.saveAccountData("src/AutoSaveAccount.txt");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                break;
-            }
-        }
-    }
+	/**
+	 * 계좌 정보를 자동으로 5초마다 저장
+	 */
+	@Override
+	public void run() {
+		while (true) {
+			accountService.saveAccountData("src/AutoSaveAccount.txt");
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+	}
 }
-
