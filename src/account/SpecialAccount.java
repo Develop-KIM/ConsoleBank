8062d65895e42db10d9eababe2bb162fc8e95c50
Kimdonghwan <kimdonghwan913@gmail.com>
2024-01-23 18:02:04 +0900

    final Commit

--- src/account/SpecialAccount.java
+++ src/account/SpecialAccount.java
@@ -1,29 +1,30 @@
 package account;
 
 public class SpecialAccount extends NormalAccount {
-    private int depositCount;
+	private int depositCount;
 
-    public SpecialAccount(String accNumber, String accName, int balance, double interestRate) {
-        super(accNumber, accName, balance, interestRate);
-        this.depositCount = 0;
-    }
+	public SpecialAccount(String accNumber, String accName, int balance, double interestRate) {
+		super(accNumber, accName, balance, interestRate);
+		this.depositCount = 0;
+	}
 
-    /**
-     * 짝수 번째의 입금마다 500원의 축하금을 지금
-     */
-    @Override
-    public void depositMoney(int amount) {
-        super.depositMoney(amount);
-        this.depositCount++;
-        if (this.depositCount % 2 == 0) {
-            this.balance += 500;System.out.println(this.depositCount + "번째 입금으로 500원의 축하금이 지급되었습니다.");
-        } else {
-            System.out.println("현재 입금 횟수: " + this.depositCount);
-        }
-    }
+	/**
+	 * 짝수 번째의 입금마다 500원의 축하금을 지금
+	 */
+	@Override
+	public void depositMoney(int amount) {
+		super.depositMoney(amount);
+		this.depositCount++;
+		if (this.depositCount % 2 == 0) {
+			this.balance += 500;
+			System.out.println(this.depositCount + "번째 입금으로 500원의 축하금이 지급되었습니다.");
+		} else {
+			System.out.println("현재 입금 횟수: " + this.depositCount);
+		}
+	}
 
-    @Override
-    public String toString() {
-        return super.toString() + "입금 횟수> " + depositCount;
-    }
+	@Override
+	public String toString() {
+		return super.toString() + "입금횟수> " + depositCount;
+	}
 }
